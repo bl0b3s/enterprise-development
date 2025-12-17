71025a0751905ed1b00a4a645696b85e7267b7b1
ahewbu <s.shalan200411@gmail.com>
2025-12-17 20:10:08 +0400

    дополнительные классы

---
+++ CarRental/CarRental/CarRental.Domain/Models/CarModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Domain_.Models;
+
+/// <summary>
+/// Модель автомобиля (справочник)
+/// </summary>
+public class CarModel
+{
+    /// <summary>
+    /// Уникальный идентификатор модели
+    /// </summary>
+    public required int ModelId { get; set; }
+
+    /// <summary>
+    /// Название модели (например, "Toyota Camry")
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// Тип привода
+    /// </summary>
+    public required DriveType DriveType { get; set; }
+
+    /// <summary>
+    /// Количество посадочных мест
+    /// </summary>
+    public required int SeatsCount { get; set; }
+
+    /// <summary>
+    /// Тип кузова
+    /// </summary>
+    public required BodyType BodyType { get; set; }
+
+    /// <summary>
+    /// Класс автомобиля
+    /// </summary>
+    public required CarClass CarClass { get; set; }
+
+    /// <summary>
+    /// Коллекция поколений этой модели
+    /// </summary>
+    public ICollection<ModelGeneration> Generations { get; set; } = new List<ModelGeneration>();
+}
+
---
+++ CarRental/CarRental/CarRental.Domain/Models/ModelGeneration.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Domain_.Models;
+
+/// <summary>
+/// Поколение модели (справочник)
+/// Пример: Toyota Camry 2020, Toyota Camry 2022
+/// </summary>
+public class ModelGeneration
+{
+    /// <summary>
+    /// Уникальный идентификатор поколения
+    /// </summary>
+    public required int GenerationId { get; set; }
+
+    /// <summary>
+    /// Внешний ключ на модель
+    /// </summary>
+    public required int ModelId { get; set; }
+
+    /// <summary>
+    /// Год выпуска
+    /// </summary>
+    public required int Year { get; set; }
+
+    /// <summary>
+    /// Объем двигателя в литрах
+    /// </summary>
+    public required double EngineVolume { get; set; }
+
+    /// <summary>
+    /// Тип коробки передач
+    /// </summary>
+    public required TransmissionType TransmissionType { get; set; }
+
+    /// <summary>
+    /// Стоимость аренды в час
+    /// </summary>
+    public required decimal HourlyRate { get; set; }
+
+    /// <summary>
+    /// Ссылка на модель (навигационное свойство)
+    /// </summary>
+    public CarModel? Model { get; set; }
+
+    /// <summary>
+    /// Коллекция автомобилей этого поколения
+    /// </summary>
+    public ICollection<Car> Cars { get; set; } = new List<Car>();
+}
